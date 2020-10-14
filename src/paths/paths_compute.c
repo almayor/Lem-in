1faaeed69ec6a4c873ee8bdad4708aa879a053ad
almayor <mayorovme@gmail.com>
2020-10-14 15:17:03 +0300

    no more leaks

--- src/paths/paths_compute.c
+++ src/paths/paths_compute.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/10 15:36:39 by unite             #+#    #+#             */
-/*   Updated: 2020/10/11 22:05:04 by user             ###   ########.fr       */
+/*   Updated: 2020/10/14 15:16:07 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ t_paths			*paths_compute(t_graph *graph, size_t nants)
 			paths_delete(paths_old);
 			paths_old = paths_new;
 		}
+		else
+			paths_delete(paths_new);
 	}
 	if (paths_new && paths_new != paths_old)
 		paths_delete(paths_new);
