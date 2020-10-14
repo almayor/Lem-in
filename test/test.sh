d60526c9fd5ac5b03026e574cfdd7dbd0948503c
almayor <mayorovme@gmail.com>
2020-10-15 01:11:46 +0300

    now tests are generating new maps on the fly

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,39 @@
 #!/bin/sh
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	generator="./generator-linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	generator="./generator-osx"
+else
+	echo "Operating system no supported."
+	exit 1
+fi
+
 echo "\n################# EXAMPLE MAPS ##################\n"
 
-maps=$(find example-maps -type f ! -path "*error-management*" -name "*\.txt")
-for map in $maps; do
-	printf "%s:\n > " $map
-	./lem-in < $map | ./check-solution
+for i in {1..20}; do
+	printf "flow-one-$i:\n > " i
+	$generator --flow-one | ./lem-in | ./check-solution
+done
+
+for i in {1..20}; do
+	printf "flow-ten-$i:\n > " i
+	$generator --flow-ten | ./lem-in | ./check-solution
+done
+
+for i in {1..20}; do
+	printf "flow-thousand-$i:\n > " i
+	$generator --flow-thousand | ./lem-in | ./check-solution
+done
+
+for i in {1..20}; do
+	printf "big-$i:\n > " i
+	$generator --big | ./lem-in | ./check-solution
+done
+
+for i in {1..20}; do
+	printf "big-superposition-$i:\n > " i
+	$generator --big-superposition | ./lem-in | ./check-solution
 done
 
 echo "\n############### ERROR MANAGEMENT ################\n"
@@ -34,9 +62,32 @@ done
 
 echo "\n#################### TIMING #####################\n"
 
-maps=$(find example-maps -type f ! -path "*error-management*" -name "big*\.txt")
-for map in $maps; do
-	printf "%s:\n > " $map
-	time ./lem-in < $map > /dev/null
+for i in {1..20}; do
+	printf "flow-one-$i:\n > " i
+	$generator --flow-one | time ./lem-in > /dev/null
+	echo
+done
+
+for i in {1..20}; do
+	printf "flow-ten-$i:\n > " i
+	$generator --flow-ten | time ./lem-in > /dev/null
+	echo
+done
+
+for i in {1..20}; do
+	printf "flow-thousand-$i:\n > " i
+	$generator --flow-thousand | time ./lem-in > /dev/null
+	echo
+done
+
+for i in {1..20}; do
+	printf "big-$i:\n > " i
+	$generator --big | time ./lem-in > /dev/null
+	echo
+done
+
+for i in {1..20}; do
+	printf "big-superposition-$i:\n > " i
+	$generator --big-superposition | time ./lem-in > /dev/null
 	echo
 done
